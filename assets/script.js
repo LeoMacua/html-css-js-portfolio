7a09e84dc21c3e5f1a13fe27dc8d1656e51ef90f
Leonard Macua <macualeonard@gmail.com>
2024-08-09 22:48:15 +0300

    Some new code

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
 function toggleMenu(){
     const menu = document.querySelector('.menu-links');
-    const icon = document.querySelector('.menu-icon');
+    const icon = document.querySelector('.hamburger-icon');
     menu.classList.toggle("open")
     icon.classList.toggle("open")
 }
@@ -8,8 +8,12 @@ function toggleMenu(){
 document.addEventListener('click', function(event) {
     const menu = document.querySelector('.menu-links');
     const icon = document.querySelector('.hamburger-icon');
+    
+    // Check if the click was outside the menu and the hamburger icon
     if (!menu.contains(event.target) && !icon.contains(event.target)) {
-        menu.classList.remove("open");
-        icon.classList.remove("open");
+        if (menu.classList.contains('open')) {
+            menu.classList.remove("open");
+            icon.classList.remove("open");
+        }
     }
 });
